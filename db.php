7919975df79fb2ae3a92237884fee9fd8e99f404
ssarruff <89736335+ssarruff@users.noreply.github.com>
2023-10-26 21:20:14 -0400

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 $servername='localhost';
 $username='root';
 $password='';
-$dbname='ssarruff';
+$dbname='';
 $con=mysqli_connect($servername,$username,$password,"$dbname");
 if(!$con){
     die('Could not connect My Sql:' .mysql_error());
